a264fc7ed523f5953328cc3ff8e510b4430927d8
wisperfrog . <galderhernandez571@gmail.com>
2024-08-01 02:40:56 -0600

    Proyecto terminado. La verdad me revolví un poco al concluir con el proyecto y no supe en que partes hacer el commit. Esencialmente funciona, se le pueden hacer acomodos al diseño

--- sript.js
+++ sript.js
@@ -1,12 +1,25 @@
 class Yhatzee {
-    constructor() {
-        this.dado1 = new Dado(1);
-        this.dado2 = new Dado(2);
-        this.dado3 = new Dado(3);
-        this.dado4 = new Dado(4);
-        this.dado5 = new Dado(5);
-        this.controlador_dados = new ControladorDados([this.dado1, this.dado2, this.dado3, this.dado4, this.dado5]);
-        this.controlador_tiradas = new ControladorTiradas;
+    constructor(num_jugadores) {
+        this.controlador_dados = new ControladorDados([new Dado(1), new Dado(2), new Dado(3), new Dado(4), new Dado(5)]);
+        this.controlador_tiradas = new ControladorTiradas(num_jugadores);
+        this.controlador_turnos = new ControladorTurno(num_jugadores);
+
+        this.lista_jugadores = [null];
+        for (let i = 1; i <= num_jugadores; i++) {
+            this.lista_jugadores.push(new ControladorScoreJugador(i));
+        }
+
+        this.controlador_ganador = new ControladorGanador;
+    }
+    
+    retornar_lista_jugadores(){
+        let lista_aux = [];
+
+        for(let i=1; i<this.lista_jugadores.length; i++){
+            lista_aux.push(this.lista_jugadores[i]);
+        }
+
+        return lista_aux;
     }
 }
 
@@ -44,6 +57,10 @@ class ControladorDados {
         });
     }
 
+    resetear_lista_reservados() {
+        this.reservados = [];
+    }
+
     verificar_todos_reservados() {
         if (this.reservados.length == 5) {
             alert("Ha reservado todos los dados, no puede hacer más tiradas.");
@@ -52,19 +69,29 @@ class ControladorDados {
             DOM_des_reservar_dados();
         }
     }
+
+    retornar_valores_actuales() {
+        let enviar = [];
+
+        this.lista.forEach(dado => {
+            enviar.push(dado.valor);
+        });
+
+        return enviar;
+    }
 }
 
 class ControladorTiradas {
     constructor() {
         this.num_tiradas = 0;
-        this.jugador = 1;
+        this.turno_jugador = 1;
     }
 
     contar_tirada() {
         this.num_tiradas++;
 
         if (this.num_tiradas <= 3) {
-            DOM_aumentar_numero_tirada(this.jugador, this.num_tiradas);
+            DOM_aumentar_numero_tirada(this.turno_jugador, this.num_tiradas);
         }
         if (this.num_tiradas >= 3) {
             DOM_desactivar_reserva();
@@ -72,6 +99,11 @@ class ControladorTiradas {
             DOM_des_reservar_dados();
         }
 
+    }
+
+    resetear_tiradas_turno_jug(jugador) {
+        this.num_tiradas = 0;
+        this.turno_jugador = jugador;
     }
 }
 
@@ -91,14 +123,34 @@ class ControladorScoreJugador {
             this.cubeta[valor]++;
         });
     }
-    
+
     calcular_score_mostrar() {
-        if (this.unos == -1) DOM_mostrar_dato_score(this.jugador, "unos", this.cubeta[1]);
-        if (this.doses == -1) DOM_mostrar_dato_score(this.jugador, "doses", this.cubeta[2] * 2);
-        if (this.treses == -1) DOM_mostrar_dato_score(this.jugador, "treses", this.cubeta[3] * 3);
-        if (this.cuatros == -1) DOM_mostrar_dato_score(this.jugador, "cuatros", this.cubeta[4] * 4);
-        if (this.cincos == -1) DOM_mostrar_dato_score(this.jugador, "cincos", this.cubeta[5] * 5);
-        if (this.seises == -1) DOM_mostrar_dato_score(this.jugador, "seises", this.cubeta[6] * 6);
+        let minimo_uno = false;
+
+        if (this.unos == -1 && this.cubeta[1] > 0) {
+            DOM_mostrar_dato_score(this.jugador, "unos", this.cubeta[1]);
+            minimo_uno = true;
+        }
+        if (this.doses == -1 && this.cubeta[2] > 0) {
+            DOM_mostrar_dato_score(this.jugador, "doses", this.cubeta[2] * 2);
+            minimo_uno = true;
+        }
+        if (this.treses == -1 && this.cubeta[3] > 0) {
+            DOM_mostrar_dato_score(this.jugador, "treses", this.cubeta[3] * 3);
+            minimo_uno = true;
+        }
+        if (this.cuatros == -1 && this.cubeta[4] > 0) {
+            DOM_mostrar_dato_score(this.jugador, "cuatros", this.cubeta[4] * 4);
+            minimo_uno = true;
+        }
+        if (this.cincos == -1 && this.cubeta[5] > 0) {
+            DOM_mostrar_dato_score(this.jugador, "cincos", this.cubeta[5] * 5);
+            minimo_uno = true;
+        }
+        if (this.seises == -1 && this.cubeta[6] > 0) {
+            DOM_mostrar_dato_score(this.jugador, "seises", this.cubeta[6] * 6);
+            minimo_uno = true;
+        }
 
         if (this.tres_tipo == -1) {
             let cumple = false, acum = 0;
@@ -108,11 +160,13 @@ class ControladorScoreJugador {
                     cumple = true;
                 }
 
-                acum += this.cubeta[i];
+                acum += this.cubeta[i] * i;
             }
 
-            if (cumple) DOM_mostrar_dato_score(this.jugador, "trestipo", acum);
-            else DOM_mostrar_dato_score(this.jugador, "trestipo", 0);
+            if (cumple) {
+                DOM_mostrar_dato_score(this.jugador, "trestipo", acum);
+                minimo_uno = true;
+            }
         }
 
         if (this.cuatro_tipo == -1) {
@@ -123,11 +177,13 @@ class ControladorScoreJugador {
                     cumple = true;
                 }
 
-                acum += this.cubeta[i];
+                acum += this.cubeta[i] * i;
             }
 
-            if (cumple) DOM_mostrar_dato_score(this.jugador, "cuatrotipo", acum);
-            else DOM_mostrar_dato_score(this.jugador, "cuatrotipo", 0);
+            if (cumple) {
+                DOM_mostrar_dato_score(this.jugador, "cuatrotipo", acum);
+                minimo_uno = true;
+            }
         }
 
         if (this.full_house == -1) {
@@ -140,16 +196,38 @@ class ControladorScoreJugador {
                 if (this.cubeta[i] == 2) {
                     cumple_par = true;
                 }
-
-                acum += this.cubeta[i];
             }
 
-            if (cumple_triada && cumple_par) DOM_mostrar_dato_score(this.jugador, "fullhouse", acum);
-            else DOM_mostrar_dato_score(this.jugador, "fullhouse", 0);
+            if (cumple_triada && cumple_par) {
+                DOM_mostrar_dato_score(this.jugador, "fullhouse", 25);
+                minimo_uno = true;
+            }
         }
 
         if (this.esc_corta == -1) {
-            let esc_index1 = true, esc_index2 = true, acum = 0;
+            let esc_index1 = true, esc_index2 = true, esc_index3 = true;
+
+            for (let i = 1; i <= 6; i++) {
+                if (i >= 1 && i <= 4) {
+                    esc_index1 = esc_index1 & this.cubeta[i];
+                }
+                if (i >= 2 && i <= 5) {
+                    esc_index2 = esc_index2 & this.cubeta[i];
+                }
+
+                if (i >= 3 && i <= 6) {
+                    esc_index3 = esc_index3 & this.cubeta[i];
+                }
+            }
+
+            if (esc_index1 || esc_index2 || esc_index3) {
+                DOM_mostrar_dato_score(this.jugador, "esccorta", 30);
+                minimo_uno = true;
+            }
+        }
+
+        if (this.esc_larga == -1) {
+            let esc_index1 = true, esc_index2 = true;
 
             for (let i = 1; i <= 6; i++) {
                 if (i >= 1 && i <= 5) {
@@ -157,25 +235,13 @@ class ControladorScoreJugador {
                 }
                 if (i >= 2 && i <= 6) {
                     esc_index2 = esc_index2 & this.cubeta[i];
-                }
-
-                acum += this.cubeta[i]
+                };
             }
 
-            if (esc_index1 || esc_index2) DOM_mostrar_dato_score(this.jugador, "esccorta", acum);
-            else DOM_mostrar_dato_score(this.jugador, "esccorta", 0);
-        }
-
-        if (this.esc_larga == -1) {
-            let cumple = true, acum = 0;
-
-            for (let i = 1; i <= 6; i++) {
-                cumple = cumple & this.cubeta[i];
-                acum += this.cubeta[i]
+            if (esc_index1 || esc_index2) {
+                DOM_mostrar_dato_score(this.jugador, "esclarga", 40);
+                minimo_uno = true;
             }
-
-            if (cumple) DOM_mostrar_dato_score(this.jugador, "esclarga", acum);
-            else DOM_mostrar_dato_score(this.jugador, "esclarga", 0);
         }
 
         if (this.yhatzee == -1) {
@@ -184,23 +250,118 @@ class ControladorScoreJugador {
             for (let i = 1; i <= 6; i++) {
                 if (this.cubeta[i] == 5) {
                     cumple = true;
-                    acum = i * 5;
                     break;
                 }
             }
 
-            if (cumple) DOM_mostrar_dato_score(this.jugador, "yahtzee", acum);
-            else DOM_mostrar_dato_score(this.jugador, "yahtzee", 0);
+            if (cumple) {
+                DOM_mostrar_dato_score(this.jugador, "yahtzee", 50);
+                minimo_uno = true;
+            }
         }
 
         if (this.chance == -1) {
             let acum = 0;
 
             for (let i = 1; i <= 6; i++) {
-                acum += this.cubeta[i];
+                acum += this.cubeta[i] * i;
             }
 
             DOM_mostrar_dato_score(this.jugador, "chance", acum);
+        }
+
+        if (!minimo_uno) {
+            this.calcular_score_mostrar_no_cumplen();
+        }
+    }
+
+    calcular_score_mostrar_no_cumplen() {
+        if (this.unos == -1 && this.cubeta[1] == 0) DOM_mostrar_dato_score(this.jugador, "unos", 0);
+        if (this.doses == -1 && this.cubeta[2] == 0) DOM_mostrar_dato_score(this.jugador, "doses", 0);
+        if (this.treses == -1 && this.cubeta[3] == 0) DOM_mostrar_dato_score(this.jugador, "treses", 0);
+        if (this.cuatros == -1 && this.cubeta[4] == 0) DOM_mostrar_dato_score(this.jugador, "cuatros", 0);
+        if (this.cincos == -1 && this.cubeta[5] == 0) DOM_mostrar_dato_score(this.jugador, "cincos", 0);
+        if (this.seises == -1 && this.cubeta[6] == 0) DOM_mostrar_dato_score(this.jugador, "seises", 0);
+
+        if (this.tres_tipo == -1) {
+            let cumple = false;
+
+            for (let i = 1; i <= 6; i++) {
+                if (this.cubeta[i] == 3) {
+                    cumple = true;
+                    break;
+                }
+            }
+
+            if (!cumple) DOM_mostrar_dato_score(this.jugador, "trestipo", 0);
+        }
+
+        if (this.cuatro_tipo == -1) {
+            let cumple = false;
+
+            for (let i = 1; i <= 6; i++) {
+                if (this.cubeta[i] == 4) {
+                    cumple = true;
+                    break;
+                }
+            }
+
+            if (!cumple) DOM_mostrar_dato_score(this.jugador, "cuatrotipo", 0);
+        }
+
+        if (this.full_house == -1) {
+            let cumple_triada = false, cumple_par = false;
+
+            for (let i = 1; i <= 6; i++) {
+                if (this.cubeta[i] == 3) {
+                    cumple_triada = true;
+                    break;
+                }
+                if (this.cubeta[i] == 2) {
+                    cumple_par = true;
+                    break;
+                }
+            }
+
+            if (cumple_triada == false || cumple_par == false) DOM_mostrar_dato_score(this.jugador, "fullhouse", 0);
+        }
+
+        if (this.esc_corta == -1) {
+            let esc_index1 = true, esc_index2 = true;
+
+            for (let i = 1; i <= 6; i++) {
+                if (i >= 1 && i <= 5) {
+                    esc_index1 = esc_index1 & this.cubeta[i];
+                }
+                if (i >= 2 && i <= 6) {
+                    esc_index2 = esc_index2 & this.cubeta[i];
+                }
+            }
+
+            if (!esc_index1 && !esc_index2) DOM_mostrar_dato_score(this.jugador, "esccorta", 0);
+        }
+
+        if (this.esc_larga == -1) {
+            let cumple = true;
+
+            for (let i = 1; i <= 6; i++) {
+                cumple = cumple & this.cubeta[i];
+            }
+
+            if (!cumple) DOM_mostrar_dato_score(this.jugador, "esclarga", 0);
+        }
+
+        if (this.yhatzee == -1) {
+            let cumple = false;
+
+            for (let i = 1; i <= 6; i++) {
+                if (this.cubeta[i] == 5) {
+                    cumple = true;
+                    break;
+                }
+            }
+
+            if (!cumple) DOM_mostrar_dato_score(this.jugador, "yahtzee", 0);
         }
     }
 
@@ -289,16 +450,85 @@ class ControladorScoreJugador {
                 break;
         }
     }
-}
 
-class ControladorTurno {
-    constructor() {
-        this.num_jugadores = 2;
+    resetear_score_mostrar() {
+        if (this.unos == -1) DOM_resetear_score(this.jugador, "unos");
+        if (this.doses == -1) DOM_resetear_score(this.jugador, "doses");
+        if (this.treses == -1) DOM_resetear_score(this.jugador, "treses");
+        if (this.cuatros == -1) DOM_resetear_score(this.jugador, "cuatros");
+        if (this.cincos == -1) DOM_resetear_score(this.jugador, "cincos");
+        if (this.seises == -1) DOM_resetear_score(this.jugador, "seises");
 
+        if (this.tres_tipo == -1) DOM_resetear_score(this.jugador, "trestipo");
+        if (this.cuatro_tipo == -1) DOM_resetear_score(this.jugador, "cuatrotipo");
+        if (this.full_house == -1) DOM_resetear_score(this.jugador, "fullhouse");
+        if (this.esc_corta == -1) DOM_resetear_score(this.jugador, "esccorta");
+        if (this.esc_larga == -1) DOM_resetear_score(this.jugador, "esclarga");
+        if (this.yhatzee == -1) DOM_resetear_score(this.jugador, "yahtzee");
+        if (this.chance == -1) DOM_resetear_score(this.jugador, "chance");
     }
 }
 
-var yahtzee_game = new Yhatzee;
+class ControladorTurno {
+    constructor(num_jugadores) {
+        this.num_jugadores = num_jugadores;
+        this.turno_jugador = 0;
+        this.turnos_totales = num_jugadores * 13;
+        this.turno_actual = 1;
+    }
+
+    retornar_turno_jugador() {
+        return this.turno_jugador + 1;
+    }
+
+    mostrar_ronda_real() {
+        if (this.turno_jugador == 0) {
+            let ronda_real = Math.floor(this.turno_actual / this.num_jugadores) + this.turno_actual % this.num_jugadores;
+
+            if (ronda_real <= 13) DOM_mostrar_ronda(ronda_real);
+        }
+    }
+
+    mostrar_turno_jugador() {
+        if (this.turno_actual <= this.turnos_totales) DOM_mostrar_turno_jugador(this.turno_jugador + 1);
+    }
+
+    finalizar_turno() {
+        this.turno_jugador = (this.turno_jugador + 1) % this.num_jugadores;
+        this.turno_actual++;
+    }
+
+    finalizar_juego() {
+        return this.turno_actual > this.turnos_totales;
+    }
+}
+
+class ControladorGanador {
+    constructor() {
+        this.lista = [];
+    }
+
+    recibir_lista_jugadores(lista_jugadores) {
+        this.lista = lista_jugadores;
+    }
+
+    calcular_ganador() {
+        let id_ganador = null, mayor_puntaje = 0;
+        //Considerar empate
+
+        this.lista.forEach(jugador_actual => {
+            jugador_actual.calc_total();
+            if (jugador_actual.tot_final > mayor_puntaje) {
+                mayor_puntaje = jugador_actual.tot_final;
+                id_ganador = jugador_actual;
+            }
+
+            DOM_terminar_juego(id_ganador);
+        });
+    }
+}
+
+var yahtzee_game = new Yhatzee(2);
 
 //Modifican/utilizan el DOM
 function DOM_cambiar_valor_dado(valor, id) {
@@ -333,15 +563,11 @@ function DOM_aumentar_numero_tirada(jugador, tiro_num) {
 }
 
 function DOM_desactivar_reserva() {
-    document.getElementById("boton_reservar").style.opacity = 0;
-    document.getElementById("boton_reservar").disabled = true;
-    document.getElementById("boton_reservar").style.cursor = "default";
+    document.getElementById("boton_reservar").style.display = "none";
 }
 
 function DOM_desactivar_tirar() {
-    document.getElementById("boton_tirar").style.opacity = 0;
-    document.getElementById("boton_tirar").disabled = true;
-    document.getElementById("boton_tirar").style.cursor = "default";
+    document.getElementById("boton_tirar").style.display = "none";
 }
 
 function DOM_des_reservar_dados() {
@@ -354,26 +580,84 @@ function DOM_des_reservar_dados() {
 }
 
 var score_ya_seleccionado = null;
+var id_score_ya_seleccionado = null;
 
 function DOM_seleccionar_score(label_id) {
+    if (score_ya_seleccionado) {
+        score_ya_seleccionado.style.transition = "0.2s";
+        score_ya_seleccionado.style.color = "#fffa68";
+        score_ya_seleccionado.style.fontSize = "15px";
+    }
+
     score_ya_seleccionado = document.getElementById(label_id);
+    id_score_ya_seleccionado = label_id;
+
+    score_ya_seleccionado.style.transition = "0.2s";
+    score_ya_seleccionado.style.color = "#da73bd";
+    score_ya_seleccionado.style.fontSize = "22px";
 }
 
 function DOM_mostrar_dato_score(jugador, elemento, valor) {
     let input_id = "check_score_" + elemento + "_j" + jugador;
     let label_id = "score_" + elemento + "_j" + jugador;
 
-    if(elemento == "total_uno_seis" || elemento == "bonus" || elemento == "total_mit_inferior" || elemento == "total_final"){
+    if (elemento == "total_uno_seis" || elemento == "bonus" || elemento == "total_mit_inferior" || elemento == "total_final") {
         document.getElementById(label_id).innerHTML = valor;
     }
-    else{
-        if(elemento == "total_mit_superior"){
-        document.getElementsByClassName(label_id).innerHTML = valor;
+    else {
+        if (elemento == "total_mit_superior") {
+            let lista = document.getElementsByClassName(label_id);
+            for (let i = 0; i < lista.length; i++) {
+                lista[i].innerHTML = valor;
+            }
         }
-        else{
+        else {
             document.getElementById(input_id).disabled = false;
             document.getElementById(label_id).innerHTML = valor;
-            document.getElementById(label_id).style.color = "#15ff00";
+            document.getElementById(label_id).style.color = "#fffa68";
         }
     }
+}
+
+function DOM_mostrar_ronda(ronda_real) {
+    document.getElementById("num_ronda").innerHTML = "número" + ronda_real;
+}
+
+function DOM_mostrar_turno_jugador(turno_jugador) {
+    document.getElementById("turno_jugador").innerHTML = "jugador" + turno_jugador;
+    document.getElementById("turno_jugador").style.color = "#fffa68";
+}
+
+function DOM_set_elementos_siguiente_jugador(jugador_anterior, jugador_actual) {
+    for (let i = 1; i <= 5; i++) {
+        document.getElementById("check_dado" + i).style.cursor = "default";
+        document.getElementById("check_dado" + i).disabled = true;
+        document.getElementById("check_dado" + i).checked = false;
+        document.getElementById("dado" + i).src = "Imagenes/signo_interr.svg";
+    }
+
+    document.getElementById("boton_reservar").style.display = "none";
+
+    document.getElementById("tirada_j" + jugador_anterior).innerHTML = "Tirada número ?";
+    document.getElementById("tirada_j" + jugador_anterior).style.display = "none";
+    document.getElementById("tirada_j" + jugador_actual).innerHTML = "Tirada número ?";
+    document.getElementById("tirada_j" + jugador_actual).style.display = "block";
+
+    document.getElementById("boton_terminar_turno_j" + jugador_anterior).style.display = "none";
+
+    document.getElementById("boton_tirar").style.display = "block";
+}
+
+function DOM_resetear_score(jugador, id) {
+    document.getElementById("check_score_" + id + "_j" + jugador).disabled = true;
+    document.getElementById("score_" + id + "_j" + jugador).innerHTML = '';
+}
+
+function DOM_terminar_juego(ganador) {
+    let mensaje = "El ganador es" + "<br>" + "el JUGADOR " + ganador.jugador + " con " + ganador.tot_final + " puntos";
+
+    document.getElementById("recuadro_ganador").innerHTML = mensaje;
+    document.getElementById("sombra_recuadro_ganador").style.display = "block";
+    document.getElementById("recuadro_ganador").style.display = "block";
+
 }
